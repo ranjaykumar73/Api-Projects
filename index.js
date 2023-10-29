17b58582e14f2da8deb7728feb62736ef74ed3c0
ranjaykumar73 <ranjaykumar73.rk@gmail.com>
2023-10-30 00:42:25 +0530

    added hotstar.html

---
+++ index.js
@@ -0,0 +1,80 @@
+// for (let i =1;i<=5;i++){
+    
+//      var hash =" "
+//     for(let j =1; j <i;j++ ){
+//         hash=hash+"# "
+//     }
+//     console.log(hash)
+        
+    
+//     //let str = '# ';
+//     //console.log(str.repeat(i))
+    
+// }
+
+
+//Use for loop to iterate from 0 to 100 and print only even number
+
+
+// for(let i = 0 ;i <= 100 ;i++){
+//     if(i % 2 === 0){
+//         console.log(i)
+//     }
+// }
+
+// use for loop to iterate from 0 to 100 and print only odd number
+
+// for(let i = 0 ; i <= 100 ;i++){
+//     if(i % 2 !== 0){
+//         console.log(i)
+//     }
+// }
+
+//Use for loop to iterate from 0 to 100 and print only prime number
+
+// for(let i = 2;i <= 100;i++){
+//     if (i % 2 === 0){
+        
+//     }
+    
+// }
+// function isPrime(){
+//     if (n/2)
+// }
+
+//Use for loop to iterate from 0 to 100 and print the sum of all number. //5050
+
+// for(var i = 0;i <=100;i++){
+//     var sum = ;
+//     console.log(sum)
+// }
+
+
+
+// Swap the data
+// [1,2,3,4,5,6];
+//[2,1,4,3,6,5]
+// let arr =[1,2,3,4,5,6]
+ 
+// for(let i = 0;i < arr.length;i++){
+//     temp = arr[1];
+//     arr[0] = temp;
+//     arr[1] = arr[0];
+//     console.log(arr)
+// }
+// ====================================
+
+// var x=1;
+// a();
+// b();
+// console.log(x)
+
+// function a(){
+//     var x=10;
+//     console.log(x)
+// }
+
+// function b(){
+//     var x=100;
+//     console.log(x)
+// }
